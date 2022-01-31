5b41892c2e4504a87673548052311d86a95210fd
allanmarmo <allanmarmo@gmail.com>
2022-01-31 15:55:03 -0300

    Initial commit

---
+++ android/app/src/main/kotlin/br/com/webmarmo/facebook_interface_aula/MainActivity.kt
@@ -0,0 +1,6 @@
+package br.com.webmarmo.facebook_interface_aula
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/telas/home.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: CustomScrollView(
+        slivers: [
+          SliverAppBar(
+            backgroundColor: Colors.white,
+            title: Text(
+              "facebook",
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
